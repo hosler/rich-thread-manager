4268d1468d2f0238bacb1923a352a19b7d27ee97
hosler <danhosler@gmail.com>
2023-01-22 10:38:14 -0600

    add setup and example

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rtm',
+    version='0.1.0',
+    description='Python thread manager via Rich enabled terminal',
+    url='https://github.com/shuds13/pyexample',
+    author='Daniel Hosler',
+    author_email='danhosler@gmail.com',
+    license='MIT',
+    packages=['rtm'],
+    install_requires=['rich>=13.2.0',
+                      'sshkeyboard>=2.3.1'
+                      ],
+)
